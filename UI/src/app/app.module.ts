65aa117af217c77f237df0597d7d43193dbcaadc
Hongfei Wu <hongfei.wu@citrix.com>
2017-01-19 16:56:44 +0800

    make search bar as component

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ApplicationComponent } from './application/application.component';
 import { ApplicationListComponent } from './application-list/application-list.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ApplicationListService } from './application-list/application-list.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     ApplicationComponent,
-    ApplicationListComponent
+    ApplicationListComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ UI/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    providers: [],
+    selector: 'search-bar',
+    styles: [require('./search-bar.component.less')],
+    template: require('./search-bar.component.html')
+})
+export class SearchBarComponent {
+
+}
