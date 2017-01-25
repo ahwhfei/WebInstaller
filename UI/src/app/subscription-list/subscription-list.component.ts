81612537b0b197f0c701c88e400bc52d2c687b6c
Hongfei Wu <hongfei.wu@citrix.com>
2017-01-25 20:31:00 +0800

    add more function in subscription list page

--- UI/src/app/subscription-list/subscription-list.component.ts
+++ UI/src/app/subscription-list/subscription-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ApplicationList } from '../application-list/application-list';
 import { SubscriptionListService } from './subscription-list.service';
+import { ApplicationListService } from '../application-list/application-list.service';
 
 @Component({
     providers: [SubscriptionListService],
@@ -13,10 +14,18 @@ export class SubscriptionListComponent implements OnInit {
 
     public subscriptionList: ApplicationList[] = [];
 
-    constructor(private subscriptionListService: SubscriptionListService) {}
+    constructor(private subscriptionListService: SubscriptionListService,
+                private applicationListService: ApplicationListService) {}
 
     ngOnInit(): void {
         this.subscriptionListService.getSubscriptionList()
-            .subscribe(data => this.subscriptionList = data);
+            .subscribe(data => {
+                this.subscriptionList = data;
+                this.subscriptionList.map((subscription: ApplicationList) => {
+                    if (!subscription.script || subscription.script.length === 0) {
+                        subscription.script = this.applicationListService.getSubscriptionScript(subscription.id || subscription._id);
+                    }
+                });
+            });
     }
 }
