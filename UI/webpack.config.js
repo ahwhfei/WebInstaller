89994086cc7afaa46029151eb4212b77b9a27c0f
Hongfei Wu <hongfei.wu@citrix.com>
2017-02-04 15:25:32 +0800

    use awesome-typescript-loader to replace ts-loader

--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
             },
             {
                 exclude: /node_modules/,
-                loader: 'ts-loader',
+                loader: 'awesome-typescript-loader',
                 test: /\.ts$/,
             },
             {
