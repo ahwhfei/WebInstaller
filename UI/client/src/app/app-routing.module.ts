1122112f8143222bb0b5b47f7af15538aff6c23c
Hongfei Wu <hongfei.wu@citrix.com>
2017-02-08 09:40:41 +0800

    split routing from root module to a standalone module

---
+++ UI/client/src/app/app-routing.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ApplicationComponent } from './application/application.component';
+import { PackageManagementComponent } from './package-management/package-management.component';
+import { SubscriptionManagementComponent } from './subscription-management/subscription-management.component';
+import { SubscriptionListComponent } from './subscription-list/subscription-list.component';
+import { CreatePackageComponent } from './create-package/create-package.component';
+import { LoginComponent } from './login/login.component';
+import { LogoffComponent } from './logoff/logoff.component';
+import { ApplicationListComponent } from './application-list/application-list.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'package',
+        component: ApplicationComponent
+    },
+    {
+        path: 'package-management',
+        component: PackageManagementComponent
+    },
+    {
+        path: 'subscription',
+        component: SubscriptionListComponent
+    },
+    {
+        path: 'subscription-management',
+        component: SubscriptionManagementComponent
+    },
+    {
+        path: 'create',
+        component: CreatePackageComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'logoff',
+        component: LogoffComponent
+    },
+    {
+        path: 'subscribe',
+        component: ApplicationListComponent
+    },
+    {
+        path: '',
+        redirectTo: '/package',
+        pathMatch: 'full'
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {}
